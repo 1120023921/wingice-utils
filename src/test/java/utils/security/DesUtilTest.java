148936211dc5e35bdd18247251e29bdc090ff30b
DoubleH <1120023921@qq.com>
2019-05-19 01:11:19 +0800

    添加加密工具类

---
+++ src/test/java/utils/security/DesUtilTest.java
@@ -0,0 +1,27 @@
+package utils.security;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author 胡昊
+ * Description:
+ * Date: 2019/5/19
+ * Time: 1:01
+ * Create: DoubleH
+ */
+public class DesUtilTest {
+
+    @Test
+    public void decrypt() {
+        String password = "0B40DA547209313EF246FBB980706F9D";
+        System.out.println(DesUtil.decrypt(password));
+    }
+
+    @Test
+    public void encrypt() {
+        String str = "2017012222";
+        System.out.println(DesUtil.encrypt(str));
+    }
+}
