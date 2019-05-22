4b14239dabab918e34cdba1e50b8b801461a85f6
DoubleH <1120023921@qq.com>
2019-05-22 21:24:25 +0800

    添加Jwt工具类

--- src/test/java/com/wingice/utils/jwt/JwtTokenUtilTest.java
+++ src/test/java/com/wingice/utils/jwt/JwtTokenUtilTest.java
@@ -3,7 +3,6 @@ package com.wingice.utils.jwt;
 import io.jsonwebtoken.Claims;
 import org.junit.Test;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,13 +18,10 @@ public class JwtTokenUtilTest {
 
     private Long expire = 3600000L;
     private String secret = "9ba45bfd500642328ec03ad8ef1b6e75";
-    private String header = "Authorization";
     String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLpmYjnjq7lj7YiLCJnZW5kZXIiOiLlpbMiLCJleHAiOjE1NTg1MzMwMDYsImpvYk51bWJlciI6IjIwMTUwMTQwOTMifQ.tFrEFDUd5ViVjm7HZ9vOe-qS-O4BArQq_5gr_T1GUXI1Gc2JRcZ32bUT9v9GgQEBGzzld6nhu9aFZ2nuhTde5Q";
-    String tokenExp = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLpmYjnjq7lj7YiLCJnZW5kZXIiOiLlpbMiLCJleHAiOjE1NTg1MzAxMjYsImpvYk51bWJlciI6IjIwMTUwMTQwOTMifQ.d29TTYmtBNZsID0816M1BwNJXCcZpuDbE81nHZ-pUV-7gEsE6RY07aRZ2dwWEEy1VGET1pLBfh83on2ZI9rYZA";
 
     @Test
     public void generateToken() {
-        Date expirationDate = new Date(System.currentTimeMillis() + expire);
         Map<String, Object> claims = new HashMap<>();
         claims.put("sub", "陈玮叶");
         claims.put("gender", "女");
