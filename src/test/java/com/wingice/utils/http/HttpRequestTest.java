4599e164fa0430b23676790e6c1edf12d6225f13
DoubleH <1120023921@qq.com>
2019-05-26 12:08:46 +0800

    添加http请求工具类

---
+++ src/test/java/com/wingice/utils/http/HttpRequestTest.java
@@ -0,0 +1,29 @@
+package com.wingice.utils.http;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author 胡昊
+ * Description:
+ * Date: 2019/5/26
+ * Time: 12:06
+ * Create: DoubleH
+ */
+public class HttpRequestTest {
+
+    @Test
+    public void sendGet() {
+        //发送 GET 请求
+        String s = HttpRequest.sendGet("http://www.baidu.com", null, null);
+        System.out.println(s);
+    }
+
+    @Test
+    public void sendPost() {
+        //发送 GET 请求
+        String s = HttpRequest.sendPost("http://www.baidu.com", null, null);
+        System.out.println(s);
+    }
+}
