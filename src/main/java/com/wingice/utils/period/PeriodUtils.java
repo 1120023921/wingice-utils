a478a0fd36f8d630d1110168b563411b71c142a3
DoubleH <1120023921@qq.com>
2019-08-12 15:25:40 +0800

    周期性日期工具类,相对星期

--- src/main/java/com/wingice/utils/period/PeriodUtils.java
+++ src/main/java/com/wingice/utils/period/PeriodUtils.java
@@ -5,9 +5,7 @@ import com.wingice.utils.period.model.*;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Calendar;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author 胡昊
@@ -101,6 +99,77 @@ public class PeriodUtils {
         return localDateList;
     }
 
+    public static List<LocalDate> absoluteMonthlyEndDate(PatternedRecurrence patternedRecurrence) {
+        final Calendar calendar = Calendar.getInstance();
+        final List<LocalDate> localDateList = new LinkedList<>();
+        calendar.setTimeInMillis(patternedRecurrence.getRange().startDate);
+        while (true) {
+            final long millis = calendar.getTimeInMillis();
+            if (millis > patternedRecurrence.getRange().endDate) {
+                break;
+            } else {
+                localDateList.add(Instant.ofEpochMilli(millis).atZone(ZoneId.systemDefault()).toLocalDate());
+                calendar.add(Calendar.MONTH, patternedRecurrence.getPattern().interval);
+            }
+        }
+        localDateList.forEach(System.out::println);
+        return localDateList;
+    }
+
+    public static List<LocalDate> absoluteMonthlyNumbered(PatternedRecurrence patternedRecurrence) {
+        final Calendar calendar = Calendar.getInstance();
+        final List<LocalDate> localDateList = new LinkedList<>();
+        calendar.setTimeInMillis(patternedRecurrence.getRange().startDate);
+        for (int i = 0; i < patternedRecurrence.getRange().numberOfOccurrences; i++) {
+            final long millis = calendar.getTimeInMillis();
+            localDateList.add(Instant.ofEpochMilli(millis).atZone(ZoneId.systemDefault()).toLocalDate());
+            calendar.add(Calendar.MONTH, patternedRecurrence.getPattern().interval);
+        }
+        localDateList.forEach(System.out::println);
+        return localDateList;
+    }
+
+    public static List<LocalDate> relativeMonthlyEndDate(PatternedRecurrence patternedRecurrence) {
+        final Calendar calendar = Calendar.getInstance();
+        final List<LocalDate> localDateList = new LinkedList<>();
+        calendar.setTimeInMillis(patternedRecurrence.getRange().startDate);
+        final LocalDate startDate = Instant.ofEpochMilli(patternedRecurrence.getRange().startDate).atZone(ZoneId.systemDefault()).toLocalDate();
+        final LocalDate endDate = Instant.ofEpochMilli(patternedRecurrence.getRange().endDate).atZone(ZoneId.systemDefault()).toLocalDate();
+        while (calendar.get(Calendar.MONTH) <= endDate.getMonthValue() - 1) {
+            for (DayOfWeek dayOfWeek : patternedRecurrence.getPattern().daysOfWeek) {
+                //调整日期至当月第一天
+                calendar.add(Calendar.DAY_OF_MONTH, -(calendar.get(Calendar.DAY_OF_MONTH) - 1));
+                //当月第一天星期
+                int firstWeekOfMonth = Instant.ofEpochMilli(calendar.getTimeInMillis()).atZone(ZoneId.systemDefault()).toLocalDate().getDayOfWeek().getValue();
+                //先增加index个周，如果当前月第一周不包含dayOfWeek,index+1
+                int nowDayOfWeek = transferFirstDayOfWeek(dayOfWeek) == 1 ? 7 : transferFirstDayOfWeek(dayOfWeek) - 1;
+                int index = nowDayOfWeek < firstWeekOfMonth ? transferWeekIndex(patternedRecurrence.getPattern().index) : (transferWeekIndex(patternedRecurrence.getPattern().index) - 1);
+                calendar.add(Calendar.WEEK_OF_MONTH, index);
+                //增加星期差额
+                int offset = nowDayOfWeek - firstWeekOfMonth;
+                long dayLong = calendar.getTimeInMillis() + offset * 24 * 60 * 60 * 1000L;
+                LocalDate day = Instant.ofEpochMilli(dayLong).atZone(ZoneId.systemDefault()).toLocalDate();
+                //如果调整日期后到下个月了，倒退7天取当月最后一个满足条件的,调整后变成上个月了，加7天取第一个满足的
+                if (day.getMonthValue() - 1 > calendar.get(Calendar.MONTH)) {
+                    day = day.minusWeeks(1);
+                } else if (day.getMonthValue() - 1 < calendar.get(Calendar.MONTH)) {
+                    day = day.plusWeeks(1);
+                }
+                //调整最后一星期情况
+                if (patternedRecurrence.getPattern().index == WeekIndex.LAST) {
+                    day = day.plusWeeks(1).getMonthValue() == day.getMonthValue() ? day.plusWeeks(1) : day;
+                }
+                if (day.isEqual(startDate) || day.isEqual(endDate) || (day.isAfter(startDate) && day.isBefore(endDate))) {
+                    localDateList.add(day);
+                }
+            }
+            calendar.set(Calendar.DAY_OF_MONTH, 1);
+            calendar.add(Calendar.MONTH, patternedRecurrence.getPattern().interval);
+        }
+        localDateList.forEach(System.out::println);
+        return localDateList;
+    }
+
     /**
      * @param dayOfWeek 星期
      * @return Calendar星期转换
@@ -124,6 +193,21 @@ public class PeriodUtils {
                 return Calendar.SATURDAY;
             default:
                 return Calendar.SUNDAY;
+        }
+    }
+
+    private static int transferWeekIndex(WeekIndex weekIndex) {
+        switch (weekIndex) {
+            case FIRST:
+                return 1;
+            case SECOND:
+                return 2;
+            case THIRD:
+                return 3;
+            case FOURTH:
+                return 4;
+            default:
+                return 4;
         }
     }
 }
--- src/test/java/com/wingice/utils/period/PeriodUtilsTest.java
+++ src/test/java/com/wingice/utils/period/PeriodUtilsTest.java
@@ -72,4 +72,54 @@ public class PeriodUtilsTest {
         patternedRecurrence.setRange(recurrenceRange);
         PeriodUtils.weeklyNumbered(patternedRecurrence);
     }
+
+    @Test
+    public void absoluteMonthlyEndDate() {
+        PatternedRecurrence patternedRecurrence = new PatternedRecurrence();
+        RecurrencePattern recurrencePattern = new RecurrencePattern();
+        recurrencePattern.type = RecurrencePatternType.ABSOLUTE_MONTHLY;
+        recurrencePattern.dayOfMonth = 12;
+        recurrencePattern.interval = 1;
+        RecurrenceRange recurrenceRange = new RecurrenceRange();
+        recurrenceRange.startDate = DateTimeUtils.getStartTimeOfDay();
+        recurrenceRange.endDate = recurrenceRange.startDate + (90L * 24 * 60 * 60 * 1000);
+        recurrenceRange.type = RecurrenceRangeType.END_DATE;
+        patternedRecurrence.setPattern(recurrencePattern);
+        patternedRecurrence.setRange(recurrenceRange);
+        PeriodUtils.absoluteMonthlyEndDate(patternedRecurrence);
+    }
+
+    @Test
+    public void absoluteMonthlyNumbered() {
+        PatternedRecurrence patternedRecurrence = new PatternedRecurrence();
+        RecurrencePattern recurrencePattern = new RecurrencePattern();
+        recurrencePattern.type = RecurrencePatternType.ABSOLUTE_MONTHLY;
+        recurrencePattern.interval = 2;
+        recurrencePattern.dayOfMonth = 12;
+        RecurrenceRange recurrenceRange = new RecurrenceRange();
+        recurrenceRange.startDate = DateTimeUtils.getStartTimeOfDay();
+        recurrenceRange.numberOfOccurrences = 10;
+        recurrenceRange.type = RecurrenceRangeType.NUMBERED;
+        patternedRecurrence.setPattern(recurrencePattern);
+        patternedRecurrence.setRange(recurrenceRange);
+        PeriodUtils.absoluteMonthlyNumbered(patternedRecurrence);
+    }
+
+    @Test
+    public void relativeMonthlyEndDate() {
+        PatternedRecurrence patternedRecurrence = new PatternedRecurrence();
+        RecurrencePattern recurrencePattern = new RecurrencePattern();
+        recurrencePattern.type = RecurrencePatternType.RELATIVE_MONTHLY;
+        recurrencePattern.daysOfWeek = Arrays.asList(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY, DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
+        recurrencePattern.interval = 1;
+        recurrencePattern.index = WeekIndex.FOURTH;
+        RecurrenceRange recurrenceRange = new RecurrenceRange();
+        recurrenceRange.startDate = DateTimeUtils.getStartTimeOfDay();
+        recurrenceRange.endDate = recurrenceRange.startDate + (90L * 24 * 60 * 60 * 1000);
+        recurrenceRange.type = RecurrenceRangeType.END_DATE;
+        patternedRecurrence.setPattern(recurrencePattern);
+        patternedRecurrence.setRange(recurrenceRange);
+        PeriodUtils.relativeMonthlyEndDate(patternedRecurrence);
+    }
+
 }
